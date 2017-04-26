a1b2aaf5f4cdec80d08f87ec042a2323feaafc9b
hrvojeanti <anticichrvoje@gmail.com>
2017-04-26 20:49:27 +0200

    nes nes nes

---
+++ resources/views/items/show.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="hr">
+    <head>
+       
+        <title>Laravel items</title>
+
+    </head>
+    <body>
+        <h2> {{ $item->title }} </h2>
+
+        <div>{{ $item->content }}</div>
+        
+
+    </body>
+</html>
